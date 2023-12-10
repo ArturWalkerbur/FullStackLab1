24fc4547490229af50c0def948b2b827829f0533
ArturWalkerbur <artur.walkerbur@gmail.com>
2023-12-10 20:34:26 +0600

    FS_FrontEnd_Angular_Login/Registration

---
+++ fs-lab1-front/src/app/pages/cars-info/cars-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-info',
+  templateUrl: './cars-info.component.html',
+  styleUrls: ['./cars-info.component.css']
+})
+export class CarsInfoComponent {
+
+}
